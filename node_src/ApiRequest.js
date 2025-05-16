a622203cb93c28f268ac6025c1e6425f14a13734
JeremiasPiontkoski <jeremiaspion@gmail.com>
2025-05-16 16:45:25 -0300

    feat: change .env vars in server.js and change api routes in ApiRequest.js

--- node_src/ApiRequest.js
+++ node_src/ApiRequest.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
 require("dotenv").config();
 
-const cloudECG_URL = process.env.CloudECG_URL;
+const FHIR_API_URL = process.env.FHIR_API_URL;
 
 const api = axios.create({
-    baseURL: cloudECG_URL
+    baseURL: FHIR_API_URL
 });
 
 module.exports = api;
--- node_src/server.js
+++ node_src/server.js
@@ -8,7 +8,7 @@ const extensionRouter = require('./routers/ExtensionRouter');
 const operationRouter = require('./routers/OperationRouter');
 require("dotenv").config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.SERVER_PORT;
 
 let allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', "*");
@@ -21,9 +21,9 @@ app.set('view engine', 'ejs');
 app.use(bp.json({limit: '50mb', extended: true}))
   .use(bp.urlencoded({limit: '50mb',extended: true }))
   .use(apiRouter)
-  .use(directRouter)
-  .use(extensionRouter)
-  .use(operationRouter)
+  .use('/ifcloud', directRouter)
+  .use('/ifcloud', extensionRouter)
+  .use('/ifcloud', operationRouter)
   .use(express.static(path.join(process.cwd(), "html_src")));
 
 app.listen(PORT, () => {
