58958e1b2975c734776423c2f9f080beaac060a0
Jeremias Piontkoski <jeremiaspion@gmail.com>
2025-11-18 12:34:38 -0300

    bug: remove form route

--- node_src/routers/OperationRouter.js
+++ node_src/routers/OperationRouter.js
@@ -2,6 +2,5 @@ const router = require("express").Router();
 const OperationController = require('../controllers/OperationController');
 
 router.post("/run_script/operation", OperationController.operationStarter);
-router.post("/myForm", OperationController.operationForm);
 
 module.exports = router;
